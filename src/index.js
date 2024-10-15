eef2e6b881c4f0eda7d410a7b37837f992fd9786
vardanmidha <111676117+vardanmidha@users.noreply.github.com>
2024-10-15 18:13:42 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Background from "./components/Background.js"
+
+import "./Reset.css"
+import "./App.css"
+
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    Background / >
+    <
+    /React.StrictMode>,
+    document.getElementById("root")
+)
